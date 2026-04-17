c2a8c435eb5fe92891bf1b5908ce689971c8f683
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2026-04-17 12:34:37 -0300

    HAHAHAHHAHAHHAHAH

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,44 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Meu primeiro app 🚀</Text>
+    <View style={styles.container}>
+      {/* Exemplo de imagem da internet */}
+      <Image 
+  source={{ uri: 'https://preview.redd.it/o-lobo-pid%C3%A3o-parou-de-ser-alimentado-e-agora-ele-est%C3%A1-v0-dforrhwdmazc1.jpeg?width=250&format=pjpg&auto=webp&s=d6e8131e5a7329138114cf49a2f7374a1db32ee7' }} // Link de imagem aleatória
+  style={styles.logo} 
+/>
+
+
+      <Text style={styles.textoGrito}>
+        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA EU N AGUENTO MAIS 🚀
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#6a787d', 
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+    borderRadius: 50, // Deixa a imagem redonda
+  },
+  textoGrito: {
+    color: '#020203', 
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 20,
+    // Efeito de contorno (stroke)
+    textShadowColor: '#fff', 
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 1,
+  },
+});
